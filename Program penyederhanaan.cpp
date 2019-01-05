3c3f6b3e44b0057bfa107ea210939d111ef219a0
AminPriadi <45525407+AminPriadi@users.noreply.github.com>
2019-01-05 19:46:33 +0700

    Add files via upload

---
+++ Program penyederhanaan.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,sisa,x,y;
+    printf("masukan bilangan = ");
+    scanf("%d",&a);
+    printf("masukan pembilang = ");
+    scanf("%d",&b);
+    sisa=a/b;
+    x=a-sisa;
+    y=a/b;
+    if(sisa==0)
+        printf("\n%d/%d",a,b,y);
+    if(sisa>0)
+        printf("\n%d/%d=%d %d/%d",a,b,y,sisa,b);
+    printf("\n");
+    return 0;
+}
